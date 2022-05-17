e105c0d3235612591b1101f54055cd6e9b2da3d1
StarTux <ths.loy@googlemail.com>
2022-05-17 12:13:45 +0200

    give

--- src/main/java/com/cavetale/ms/MassStorageAdminCommand.java
+++ src/main/java/com/cavetale/ms/MassStorageAdminCommand.java
@@ -11,7 +11,9 @@ import com.winthier.playercache.PlayerCache;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import static net.kyori.adventure.text.Component.join;
 import static net.kyori.adventure.text.Component.text;
+import static net.kyori.adventure.text.JoinConfiguration.noSeparators;
 import static net.kyori.adventure.text.event.HoverEvent.showText;
 import static net.kyori.adventure.text.format.NamedTextColor.*;
 
@@ -34,8 +36,16 @@ public final class MassStorageAdminCommand extends AbstractCommand<MassStoragePl
             .playerCaller(this::storableSerialize);
         CommandNode playerNode = rootNode.addChild("player")
             .description("Player subcommands");
+        playerNode.addChild("give").arguments("<player> <type> <name> <amount>")
+            .description("Add items to player's storage")
+            .completers(PlayerCache.NAME_COMPLETER,
+                        CommandArgCompleter.enumLowerList(StorageType.class),
+                        CommandArgCompleter.supplyList(() -> plugin.index.allSqlNames()),
+                        CommandArgCompleter.integer(i -> i > 0))
+            .senderCaller(this::playerGive);
         playerNode.addChild("giveone").arguments("<player>")
             .description("Give one of each storable")
+            .completers(PlayerCache.NAME_COMPLETER)
             .senderCaller(this::playerGiveOne);
     }
 
@@ -43,9 +53,9 @@ public final class MassStorageAdminCommand extends AbstractCommand<MassStoragePl
         if (args.length != 2) return false;
         StorageType type = StorageType.require(args[0]);
         String sqlName = args[1];
-        StorableItem item = plugin.index.get(type, sqlName);
-        if (!item.isValid()) throw new CommandWarn("Invalid item: " + type + ", " + sqlName);
-        sender.sendMessage(text(item.toString(), AQUA));
+        StorableItem storable = plugin.index.get(type, sqlName);
+        if (!storable.isValid()) throw new CommandWarn("Invalid storable: " + type + ", " + sqlName);
+        sender.sendMessage(text(storable.toString(), AQUA));
         return true;
     }
 
@@ -55,6 +65,26 @@ public final class MassStorageAdminCommand extends AbstractCommand<MassStoragePl
         player.sendMessage(text(serialized, AQUA)
                            .hoverEvent(showText(text(serialized, AQUA)))
                            .insertion(serialized));
+    }
+
+    private boolean playerGive(CommandSender sender, String[] args) {
+        if (args.length != 4) return false;
+        PlayerCache target = PlayerCache.require(args[0]);
+        MassStorageSession session = plugin.sessions.get(target.uuid);
+        if (session == null || !session.isEnabled()) {
+            throw new CommandWarn("Session not loaded: " + target.name);
+        }
+        StorageType type = StorageType.require(args[1]);
+        String sqlName = args[2];
+        StorableItem storable = plugin.index.get(type, sqlName);
+        if (!storable.isValid()) throw new CommandWarn("Invalid storable: " + type + ", " + sqlName);
+        int amount = CommandArgCompleter.requireInt(args[3], i -> i > 0);
+        session.insert(storable, amount);
+        sender.sendMessage(join(noSeparators(),
+                                text("Gave " + amount + "x", AQUA),
+                                storable.getDisplayName(),
+                                text(" to " + target.name, AQUA)));
+        return true;
     }
 
     private boolean playerGiveOne(CommandSender sender, String[] args) {
